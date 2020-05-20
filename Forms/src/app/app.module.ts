75651b4329e0859fb61bd629bb456359b5c14e94
Italo Nunes Leite <italonleite@gmail.com>
2020-05-20 00:07:37 -0300

    - inserindo forms cadastro ReactiveForms

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -10,7 +10,8 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
-import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { CadastroComponent } from './demo/reactiveForms/cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
@@ -20,10 +21,12 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
     FooterComponent,
     SobreComponent,
     CadastroComponent
+  
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
   providers: [
