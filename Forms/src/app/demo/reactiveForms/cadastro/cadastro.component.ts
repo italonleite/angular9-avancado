2f6ad5d22718e1f30e4aa81e7d733724b2798a59
Italo Nunes Leite <italonleite@gmail.com>
2020-05-23 14:20:42 -0300

    - substrituindo formGroup para group FormBuilder - substituindo cada instancia (new) para array

--- Forms/src/app/demo/reactiveForms/cadastro/cadastro.component.ts
+++ Forms/src/app/demo/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,16 +9,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+
+  }
 
   ngOnInit() {
-    this.cadastroForm = new FormGroup({
+    this.cadastroForm = this.fb.group({
       //elementos do formulário
      
-      nome: new FormControl(''),
-      cpf: new FormControl(''),
-      email: new FormControl('')
-
+      nome: [''],
+      cpf: [''],
+      email: [''],
     });
   }
 
